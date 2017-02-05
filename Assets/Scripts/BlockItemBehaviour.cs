c9036a768bdb33ad49d1d67b607ac6dd61f376d7
135yshr <isago@on-s.co.jp>
2017-02-05 15:46:49 +0900

    chg link to BlockItem object

--- Assets/Scripts/BlockItemBehaviour.cs
+++ Assets/Scripts/BlockItemBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockItemB : MonoBehaviour {
+public class BlockItemBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
