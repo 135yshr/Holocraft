c839c6b8fd4be4bfe14f90f50227b27d92b1a224
135yshr <isago@on-s.co.jp>
2017-02-06 13:35:44 +0900

    done make cube test and spatial processing

--- Assets/Scripts/BlockItemBehaviour.cs
+++ Assets/Scripts/BlockItemBehaviour.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockItemBehaviour : MonoBehaviour {
+public class BlockItemBehaviour : MonoBehaviour
+{
+    public GameObject blockPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var headPosition = Camera.main.transform.position;
+        //var gazeDirection = Camera.main.transform.forward;
+
+        //RaycastHit hitInfo;
+        //if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        //{
+        //    Debug.Log("position: " + hitInfo.point);
+        //    //blockPrefab.transform.localScale = new Vector3(hitInfo.point.x, hitInfo.point.y + 1, hitInfo.point.z);
+        //    Instantiate(blockPrefab,
+        //        new Vector3(hitInfo.point.x, hitInfo.point.y + 1, hitInfo.point.z),
+        //        Quaternion.Euler(0, 0, 0));
+        //}
+    }
 }
