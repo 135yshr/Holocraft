010bbc6b2aa5e0f19712a9d4ec22d4a3139eddb3
135yshr <isago@on-s.co.jp>
2017-02-09 14:40:42 +0900

    Initial commit

--- Assets/Scripts/MinecraftConnector.cs
+++ Assets/Scripts/MinecraftConnector.cs
@@ -1,16 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
 
-public class MinecraftConnector : MonoBehaviour {
+public class MinecraftConnector : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Tooltip("The connection port on the machine to use.")]
+    public int ConnectionPort = 25566;
+
+#if UNITY_EDITOR
+
+    /// <summary>
+    /// Listens for network connections over TCP.
+    /// </summary> 
+    private TcpListener networkListener;
+
+    /// <summary>
+    /// Keeps client information when a connection happens.
+    /// </summary>
+    private TcpClient networkClient;
+
+    /// <summary>
+    /// Tracks if a client is connected.
+    /// </summary>
+    private bool ClientConnected = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // Setup the network listener.
+        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+        networkListener = new TcpListener(localAddr, ConnectionPort);
+        networkListener.Start();
+
+        // Request the network listener to wait for connections asynchronously.
+        AsyncCallback callback = new AsyncCallback(OnClientConnect);
+        networkListener.BeginAcceptTcpClient(callback, this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ClientConnected == false)
+        {
+            return;
+        }
+    }
+
+    /// <summary>
+    /// Called when a client connects.
+    /// </summary>
+    /// <param name="result">The result of the connection.</param>
+    void OnClientConnect(IAsyncResult result)
+    {
+        if (result.IsCompleted)
+        {
+            networkClient = networkListener.EndAcceptTcpClient(result);
+            if (networkClient != null)
+            {
+                Debug.Log("Connected");
+                ClientConnected = true;
+            }
+        }
+    }
+#endif
 }
